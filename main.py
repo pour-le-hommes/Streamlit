a9c592e490de2661bba599791147ccdb001daafe
Pour Le Hommes <oga_takashi@yahoo.com>
2024-06-02 23:51:38 +0800

    Update homepage and a few settings for ease of update

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from utils.navbar import Navbar
-from pages.skills import skillspage
 
 def main():
     Navbar()
 
-    st.title(f'🔥 My Main Page')
+    st.title(f'🔥 **TERRA17066**')
     st.logo("data/itb.jpg")
     st.sidebar.markdown("Welcome! My personal information page!")
     st.sidebar.title("Navigation")
@@ -14,8 +13,18 @@ def main():
 
 
     def home():
-        st.title("Home")
-        st.write("Welcome to the home page!")
+        st.write("Welcome to the home page! I'm a machine learning engineer who dwells in hugging face or kaggle. You'll see that I'm working on a lot of LLMs (or ChatGPT-esque for those people who are normal and don't know any jargons)")
+        st.write("I'm working on this website because of it's ease with python's data libraries. Be sure to come here often because I'll update this alot 🤘")
+        st.info("What do I have in store for you? Scroll down 👇")
+
+        st.header("*Badan Pusat Statistik Graph* with LLM Analysis")
+        st.image("data/pendidikan.png")
+        st.image("data/pengangguran.png")
+
+        st.header("**Personal Chatbot** (so long as you know the password 😆)")
+        st.image("data/terra_chatbot.png")
+        with st.expander("For the password"):
+            st.markdown(" if you have colleague from ITB, ask them lmao, or just google them")
 
     if page == "Home":
         home()
