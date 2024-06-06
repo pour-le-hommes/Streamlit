bd42638f580b278afaadce4e0c0b870ea8307978
Pour Le Hommes <oga_takashi@yahoo.com>
2024-06-06 18:50:54 +0800

    Checkpoint

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from utils.navbar import Navbar
 from utils.Chatbot_config import text_stream
-from utils.homepage_function import project_list, show_more, for_the_password, download_button
+from utils.homepage_function import project_list, show_more, download_button, first_show_me, show_more_more, email_button
 import asyncio
+from streamlit_extras.stylable_container import stylable_container
+
 st.set_page_config(
     page_title='Home',
     page_icon='🔥',
@@ -13,22 +15,46 @@ st.set_page_config(
 with open("style.css") as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
+SOCIAL_MEDIA = {
+    "Kaggle": "https://www.kaggle.com/ogatakashi",
+    "LinkedIn": "https://www.linkedin.com/in/bima-ilyasa/",
+    "GitHub": "https://github.com/pour-le-hommes",
+    "Instagram": "https://www.instagram.com/bimoilyasa/",
+}
+
 if "model_picked" not in st.session_state:
     st.session_state.model_picked = "@cf/meta/llama-3-8b-instruct"
 
 if "first_home" not in st.session_state:
     st.session_state.first_home = False
 
+if "second_home" not in st.session_state:
+    st.session_state.second_home = False
+
+if "first_proj" not in st.session_state:
+    st.session_state.first_proj = ""
+
+if "second_proj" not in st.session_state:
+    st.session_state.second_proj = ""
+
+if "third_proj" not in st.session_state:
+    st.session_state.third_proj = ""
+
 Navbar()
 # st.markdown('🔥 **TERRA17066**')
-colhead1,_,_, colhead2 = st.columns(4)
+colhead1, colhead2 = st.columns(2,gap="small")
 with colhead1:
-    st.markdown("<medium>I'm Bima, Data Enthusiast, AI Engineer</medium>", unsafe_allow_html=True)
+    st.image("data/34.jpg", width=300)
+
 with colhead2:
+    st.markdown("<medium>**Bima Ilyasa Rachmanditya**</medium>", unsafe_allow_html=True)
+    st.markdown("<medium>AI Engineer, Data enthusiast</medium>", unsafe_allow_html=True)
+    # st.button("📫 Email me",key="email",type="secondary")
+    email_button()
     download_button()
+
 st.markdown("<h1>Welcome to my machine learning journey! 🚀 I'm thrilled to share my passion for harnessing the power of Large Language Models (LLMs) with you. As a machine learning engineer, I'm constantly exploring innovative ways to analyze and visualize data, with a focus on Government Data Analysis, Cloudflare's AI Integration, and making Accessible AI Tools.</h1>",unsafe_allow_html=True)
-# st.image("data/itb.jpg",caption="ITB 1978 with AI enhancement",width=500)
-st.sidebar.write("Welcome! My personal information page!",unsafe_allow_html=True)
+st.write('\n')
 
 # res = st.empty()
 # asyncio.run(welcoming_prompt(res))
@@ -38,29 +64,36 @@ if st.session_state.first_home==False:
 
 if st.session_state.first_home==True:
     st.divider()
-    
-    cols1,cols2 = st.columns(2)
+    project1,project2,project3 = first_show_me()
 
-    with cols1:
-        project1 = st.empty()
-        st.page_link('pages/indonesia_data.py', label="Indonesia's Data 🇮🇩", icon='➡️')
-        project2 = st.empty()
-        st.page_link("pages/cloudflare_main_page.py", label="Testing Cloudflare LLMs ☁️", icon='➡️')
-        project3 = st.empty()
-        st.page_link('pages/terra.py', label='TERRA Impersonator 💯', icon='➡️')
+    if st.session_state.first_proj=="":
+        asyncio.run(project_list(project1,project2,project3))
 
-    with cols2:
-        st.image("data/pengangguran.png",caption="Hover to find the button to full screen")
-        st.markdown("<h2></h2>",unsafe_allow_html=True)
-        st.markdown("<medium></medium>",unsafe_allow_html=True)
-        st.markdown('#')
-        st.image("data/cloudflare.png",caption="Hover to find the button to full screen")
-        st.markdown("<h1></h1>",unsafe_allow_html=True)
-        st.markdown("<medium></medium>",unsafe_allow_html=True)
-        st.markdown("<medium></medium>",unsafe_allow_html=True)
-        st.image("data/terra_chatbot.png",caption="Hover to find the button to full screen")
-        for_the_password()
+    if st.session_state.second_home==False:
+        show_more_more()
 
-    asyncio.run(project_list(project1,project2,project3))
+    if st.session_state.second_home==True:
+        st.divider()
+        st.markdown("<h2>My Socials</h2>",unsafe_allow_html=True)
+        cols = st.columns(len(SOCIAL_MEDIA))
+        for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+            with stylable_container(
+                key=f"link_button{index}",
+                css_styles="""
+                    link_button {
+                        background-color: 1e1e1e1e;
+                        color: #ffffff;
+                        border-radius: 50px;
+                        transition-duration: 0.4s;
+                    }
+                    link_button:hover {
+                    background-color: blue;
+                    color: white;
+                    border-style: dotted;
+                    border-color: blue;
+                    }
+                    """,
+            ):
+                cols[index].link_button(platform,link,use_container_width=True)
 # if __name__ == '__main__':
 #     main()
